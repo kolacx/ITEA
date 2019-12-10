377c5fbfcbc216425aa8c529e4c298f5d4afbd28
koalcx <lisicun.aleksandr@gmail.com>
2019-12-10 18:15:51 +0200

    task2 fin

--- lesson2/task2.py
+++ lesson2/task2.py
@@ -5,21 +5,21 @@ class Market:
     def __init__(self, name, sale_product):
         self._name = name
         self._sale_product = sale_product
-        self.total += sale_product
+        Market.total += sale_product
 
     def set_product(self, add_product):
         self._sale_product += add_product
+        Market.total += add_product
 
-    def show_total_sale(self):
-        print(self.total)
 
 market1 = Market('ATB', 200)
 
-print(market1._name)
-print(market1._sale_product)
-
 market1.set_product(20)
 
-print(market1._sale_product)
+print(Market.total)
 
-Market.show_total_sale(market1)
+market2 = Market('Silpo', 400)
+
+market2.set_product(20)
+
+print(Market.total)
