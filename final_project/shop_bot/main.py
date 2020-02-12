9ddfca3f0441bfa53bd8c98df7cfac9b88ead05e
koalcx <lisicun.aleksandr@gmail.com>
2020-02-12 21:20:34 +0200

    ss6

--- final_project/shop_bot/main.py
+++ final_project/shop_bot/main.py
@@ -19,7 +19,7 @@ def webhook():
     if request.headers.get('content-type') == 'application/json':
 
         json_string = request.get_data().decode('utf-8')
-        update = types.Update.de_json(json_string)
+        update = Update.de_json(json_string)
         bot.process_new_updates([update])
         return ''
 
