0b262393d93b883202001a5d2d1ebe38e8315e46
koalcx <lisicun.aleksandr@gmail.com>
2020-02-12 21:03:43 +0200

    set serbver 5

--- final_project/shop_bot/main.py
+++ final_project/shop_bot/main.py
@@ -19,7 +19,7 @@ def webhook():
     if request.headers.get('content-type') == 'application/json':
 
         json_string = request.get_data().decode('utf-8')
-        update = telebot.types.Update.de_json(json_string)
+        update = types.Update.de_json(json_string)
         bot.process_new_updates([update])
         return ''
 
