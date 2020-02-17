bd6bc61715776eb46c7dbb5fc5dae1f78774343b
koalcx <lisicun.aleksandr@gmail.com>
2020-02-17 21:36:08 +0200

    lesson

--- final_project/shop_bot/main.py
+++ final_project/shop_bot/main.py
@@ -106,12 +106,3 @@ Server -
 
 if __name__ == '__main__':
     utils.start_bot()
-    # import time
-    # print('STARTED')
-    # bot.remove_webhook()
-    # time.sleep(1)
-    # bot.set_webhook(
-    #     url=WEBHOOK_URL,
-    #     certificate=open('nginx-selfsigned.crt', 'r')
-    #     )
-    # app.run(host='127.0.0.1', port=5000, debug=True)
