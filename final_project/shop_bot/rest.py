c508a01840a00d9914a4666d522f4476865a0116
kolacx <lisicun.aleksandr@gmail.com>
2020-02-26 17:58:25 +0200

    fix url

--- final_project/shop_bot/rest.py
+++ final_project/shop_bot/rest.py
@@ -6,8 +6,8 @@ from flask_restful import Api
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(CategoryResource, '/category/', '/category/<string:cat_id>')
-api.add_resource(ProductResource, '/product/', '/product/<string:prod_id>')
+api.add_resource(CategoryResource, '/api/category/', '/category/<string:cat_id>')
+api.add_resource(ProductResource, '/api/product/', '/product/<string:prod_id>')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=6000, debug=True)
